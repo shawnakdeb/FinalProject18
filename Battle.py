d92d1bdbb5daa1bacdd15f66993ccc86d653647b
roronyc19 <roronyc19@users.noreply.github.com>
2019-01-17 13:05:58 -0500

    Created Battle

---
+++ Battle.py
@@ -0,0 +1,29 @@
+from Pokemon import Pokemon
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((400, 400))
+running = True
+
+@staticmethod
+def turn(atkP, defP):
+   pass
+@staticmethod
+def battle(userP, compP):
+   while userP.hp > 0 and compP.hp > 0:
+       if userP.spd >= compP.spd:
+           turn(userP, compP)
+           if compP.hp <= 0:
+               break
+           turn(compP, userP)
+       else:
+           turn(compP, userP)
+           if compP.hp <= 0:
+               break
+           turn(userP, compP)
+  
+while running:
+
+   for e in pygame.event.get():    # get each event in the event queue...
+    if e.type == pygame.QUIT:   # ...and if that event is QUIT...
+       running = False         # ......set running to False so the main loop ends
+
