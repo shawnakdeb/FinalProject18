8dc5b0b64e14ece1711b609f9be5ed3152dc10c1
roronyc19 <roronyc19@users.noreply.github.com>
2019-01-28 14:14:53 -0500

    Final Commit
    Fixed final bugs

--- Pokemon.py
+++ Pokemon.py
@@ -2,7 +2,7 @@ import math, random, pygame
 pygame.init()
 
 class Pokemon:
-
+    """Initializes Pokemon object with base stats, actual stats, experience points, sprite, level, moves, hit points"""
     def __init__(self, species, base_hp, base_atk, base_defe, base_spd, base_exp, sprite, exp, p_type, moves):
         self.species = species
         self.base_hp = base_hp
@@ -26,26 +26,32 @@ class Pokemon:
         self.isFainted = False
 
 def copy(Poke):
+    """Creates a clone of a Pokemon"""
     return Pokemon(Poke.species, Poke.base_hp, Poke.base_atk, Poke.base_defe, Poke.base_spd, Poke.base_exp, Poke.sprite, Poke.exp, Poke.p_type, Poke.moves)
 
 def set_sprite(self, img):
+    """Sets a Pokemon's sprite to the given image"""
     self.sprite = img
 
 def calcLevel(pok):
+    """Given some value of exp points, calculates the level based on base experience"""
     while pok.exp > 4 / 5 * pok.lvl * pok.lvl * pok.lvl and pok.lvl < 100:
          pok.lvl+=1        
 
 def expGain(atkPok, oppPok):
-     atkPok.exp += 2.1 * oppPok.base_exp * oppPok.lvl / 7
-     calcLevel(atkPok)
+    """Gains experience after defeating opponent"""
+    atkPok.exp += 2.1 * oppPok.base_exp * oppPok.lvl / 7
+    calcLevel(atkPok)
 
 def calcStats(pok):
+    """Calculates stats based on base stats and level"""
     pok.atk = math.floor( ((pok.base_atk+25)*2+25)*pok.lvl/100.0 ) + 5
     pok.defe = math.floor( ((pok.base_defe+25)*2+25)*pok.lvl/100.0 ) + 5
     pok.spd = math.floor( ((pok.base_spd+25)*2+25)*pok.lvl/100.0 ) + 5
     pok.max_hp = math.floor( ((pok.base_hp+25)*2+25)*pok.lvl/100.0 ) + pok.lvl + 10
 
 def initialize(pok):
+    """Calculates level and stats of a Pokemon and sets hp to max"""
     calcLevel(pok)    
     pok.atk = math.floor( ((pok.base_atk+25)*2+25)*pok.lvl/100.0 ) + 5
     pok.defe = math.floor( ((pok.base_defe+25)*2+25)*pok.lvl/100.0 ) + 5
@@ -54,15 +60,18 @@ def initialize(pok):
     pok.hp = pok.max_hp
 
 def restore_hp(pok):
+    """refills a Pokemon's hp"""
     pok.hp = pok.max_hp
 
 def update_stats(pok, deafPok):
+    """Updates a Pokemon's stats"""
     expGain(pok,deafPok)
     hpLost = pok.max_hp - pok.hp
     calcStats(pok)
     pok.hp = pok.max_hp - hpLost
 
 def new_arbok():
+    """Creates a new Arbok"""
     from BattleMain import rem_background, double_size
     arbok = Pokemon("Arbok", 60, 95, 69, 80, 157, "arbok front.png", 60, "Poison", ["Sludge Bomb", "Brick Break", "Earthquake", "Rock Slide"])
     rem_background(arbok)
